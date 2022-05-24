eb429bf5a2e9fde5d1f3d53920faf4ac6986e609
StoyanNakov <105935309+StoyanNakov@users.noreply.github.com>
2022-05-24 12:28:31 +0300

    test new

---
+++ Conditional Statements Advanced - Lab/DayOfWeek/DayOfWeek/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ad
+                asdasd
+                ads
+
+                adads
+
+                asd
+        }
+    }
+}
